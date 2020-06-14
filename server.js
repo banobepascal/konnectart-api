e2fceb789c73237d3e2229c176449f3b7eb95010
banobepascal <banobepascal@gmail.com>
2020-06-14 22:45:37 +0300

    ft - created app server and configured scripts

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port} ...`)
+);
+
+module.exports = server;
