60e7a0ff48c7fa66be30b646e4d8478f8304ccc5
banobepascal <banobepascal@gmail.com>
2020-06-15 19:57:31 +0300

    enforcing code style bywith eslint

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-// const express = require("express");
 import express from 'express';
+
 const app = express();
 
 const port = 3000;
-const server = app.listen(port, () =>
-  console.log(`Listening on port ${port} ...`)
-);
+const server = app.listen(port, () => console.log(`Listening on port ${port} ...`));
 
 export default server;
