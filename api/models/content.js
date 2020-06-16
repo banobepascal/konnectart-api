10dde60d4213451ad28191163f21fdabcbce3854
banobepascal <banobepascal@gmail.com>
2020-06-16 16:55:45 +0300

    creating mongoose model for image

---
+++ api/models/content.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const Image = mongoose.model('Image', new mongoose.Schema({
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
+}));
+
+function validateImage(image) {
+  const schema = {
+    name: Joi.buffer().required(),
+  };
+
+  return Joi.validate(image, schema);
+}
+
+// exports.imageSchema = imageSchema;
+exports.Image = Image;
+exports.validate = validateImage;
