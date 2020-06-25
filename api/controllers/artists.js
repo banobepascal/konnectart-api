c877c85e8fa0ed61fcaf502850b9e721605e4519
banobepascal <banobepascal@gmail.com>
2020-06-25 13:39:49 +0300

    ch - refactored some code and fixed errors

--- api/controllers/artists.js
+++ api/controllers/artists.js
@@ -3,22 +3,29 @@ import Artist from '../models/artist';
 class Artists {
   async getAllArtists(req, res) {
     const artists = await Artist.find();
-    const artist = artists.map((username) => username);
-    res.status(200).json({
+    const data = [];
+    artists.forEach((artist) => {
+      const fullName = [artist.firstname, artist.lastname];
+      data.push({
+        fullName: fullName.join(' '),
+        username: artist.username,
+      });
+    });
+    return res.status(200).json({
       status: 'OK',
-      artists: artist,
+      artists: data,
     });
   }
 
   async getArtistByUsername(req, res) {
     const username = await Artist.findOne({ username: req.params.username });
-
+    const fullName = [username.firstname, username.lastname];
     return res.status(200).json({
       status: 'OK',
       artist: {
-        firstname: username.firstname,
-        lastname: username.lastname,
+        fullName: fullName.join(' '),
         username: username.username,
+        bio: username.bio,
       },
     });
   }
