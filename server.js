31ebe4e698c92d54ab82e5e82a12756863c91e7f
banobepascal <banobepascal@gmail.com>
2020-07-02 23:23:50 +0300

    bg - fixed failing request to db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import config from './api/config/config';
 
 const appDebug = require('debug')('app:startup');
 
-require('./api/startup/db')();
+require('./api/startup/db')
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => appDebug(`Listening on port ${port}...`));
